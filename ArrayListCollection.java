793adf9407b9648cc1be9e436fe87f86c011fced
shebbarjk <92441240+shebbarjk@users.noreply.github.com>
2021-10-22 11:21:33 +0530

    Create ArrayListCollection.java

---
+++ ArrayListCollection.java
@@ -0,0 +1,20 @@
+package training.collectionsAssesment;
+import java.util.*;
+
+public class ArrayListCollection {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list=new ArrayList<String>(); 
+		list.add("Ravi");
+		list.add("Vijay");  
+		list.add("Ravi");  
+		list.add("Ajay");  
+		  
+		Iterator itr=list.iterator();  
+		while(itr.hasNext()){  
+			System.out.println(itr.next());  
+		}
+	}
+
+}
